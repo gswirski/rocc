f99870aae21ab70420b7946a5be39610db8f798c
Grzegorz Świrski <grzegorz@swirski.name>
2021-07-04 19:59:37 +0200

    turn off aggressive locking

--- Sources/Helpers/PTP/Packets/EndDataPacket.swift
+++ Sources/Helpers/PTP/Packets/EndDataPacket.swift
@@ -52,7 +52,7 @@ struct EndDataPacket: Packetable {
     }
     
     var description: String {
-        return "\(length) | n:\(name) | t:\(transactionId) | \(data.toHex)"
+        return "\(length) | n:\(name) | t:\(transactionId) | \(data.toHex.trunc(length: 50))"
 
    }
 }
